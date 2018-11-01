f1bf2bafce9d24cad7f55dadfdea466ce321be90
Sinook Kang <sokang337@gmail.com>
2018-11-01 15:57:58 +0900

    update

--- webapps/ROOT/assets/js/gesture/gesture.js
+++ webapps/ROOT/assets/js/gesture/gesture.js
@@ -115,7 +115,7 @@ function playAnimation(fbxName) {
         //camera.position.set(100, 2000, 5500);
         camera = new THREE.PerspectiveCamera(45, 340 / 650, 3, 200000);     //케릭터 생성위치(width/height)
         //camera.position.set(100, 2000, 6500);
-        camera.position.set(100, 2200, 6000);
+        camera.position.set(100, 2200, 6100);
 
         //궤도 제어는 카메라가 표적 주위를 도는 것을 허용합니다. 
         //controls = new THREE.OrbitControls(camera);
