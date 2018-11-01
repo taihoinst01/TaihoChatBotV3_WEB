22fa5fc1854aac047d603be5edd42d5c4840ca94
Sinook Kang <sokang337@gmail.com>
2018-11-01 17:23:58 +0900

    update

--- webapps/ROOT/assets/js/gesture/gesture.js
+++ webapps/ROOT/assets/js/gesture/gesture.js
@@ -115,7 +115,7 @@ function playAnimation(fbxName) {
         //camera.position.set(100, 2000, 5500);
         camera = new THREE.PerspectiveCamera(45, 340 / 650, 3, 200000);     //케릭터 생성위치(width/height)
         //camera.position.set(100, 2000, 6500);
-        camera.position.set(100, 2200, 6500);
+        camera.position.set(100, 2400, 6500);
 
         //궤도 제어는 카메라가 표적 주위를 도는 것을 허용합니다. 
         //controls = new THREE.OrbitControls(camera);
@@ -216,8 +216,9 @@ function playAnimation(fbxName) {
         //console.log($('.gesture-wrapper').css('height'));
 
         var _gestureHeight = parseInt($('.gesture-wrapper').css('height'));
-        renderer.setSize(340, _gestureHeight);     //챗팅창에 고려한 canvas 사이즈 임의 조정
-        
+        //renderer.setSize(340, _gestureHeight);     //챗팅창에 고려한 canvas 사이즈 임의 조정
+        renderer.setSize(450, _gestureHeight);     //챗팅창에 고려한 canvas 사이즈 임의 조정
+
         container.append(renderer.domElement);
         //window.addEventListener('resize', onWindowResize, false); //resize 안되게 주석
 
