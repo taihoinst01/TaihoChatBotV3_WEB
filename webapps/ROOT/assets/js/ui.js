6704145a5ff5a2948216051c5190c3c9e552babd
장 상련 <srjang@taihoinst.com>
2018-11-05 13:16:21 +0900

    현재위치사용승인

--- webapps/ROOT/assets/js/ui.js
+++ webapps/ROOT/assets/js/ui.js
@@ -272,7 +272,8 @@ $(function () {
 
     //현재위치사용승인
     //$(document).on('click', '.wc-card > .wc-card-buttons > li > button:contains("현재 위치 사용 승인")', function () {
-    $(document).on('click', '.wc-list button:contains("현재 위치 사용 승인")', function () {
+    //$(document).on('click', '.wc-list button:contains("현재 위치 사용 승인")', function () {
+    $(document).on('click', '.ac-pushButton:contains("현재 위치 사용 승인")', function () {
         navigator.geolocation.getCurrentPosition(function (position) {
             var pos = { lat: position.coords.latitude, lng: position.coords.longitude };
             //alert("현재 위치: 위도(" + pos.lat + "), 경도(" + pos.lng + ")");
