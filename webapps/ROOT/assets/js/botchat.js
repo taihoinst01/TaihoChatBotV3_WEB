d8e90f2c63e5b1cd9ea03aaa6fd223f4d4b7b9fa
Sinook Kang <sokang337@gmail.com>
2018-11-15 11:23:50 +0900

    update

--- webapps/ROOT/assets/js/botchat.js
+++ webapps/ROOT/assets/js/botchat.js
@@ -13454,6 +13454,7 @@
                             d.appendChild(this._element, o), n++
                         }
                     }
+                this._element.innerHTML = this._element.innerHTML.replace("/n", "</br>");
                 return this._element
             }, e.prototype.getBackgroundColor = function() {
                 return null
